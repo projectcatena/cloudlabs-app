26fa3262c7a7e8f8787b6c710f1b7be195e99592
Kai <kaizhetan@yahoo.com>
2023-06-10 14:34:44 +0800

    test: Init uni test with Jest and RTL

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import Navbar from '../components/modules/navbar'
 import { Inter } from 'next/font/google'
 
@@ -18,12 +19,12 @@ export default function Home() {
               <p className="mt-3 text-lg text-gray-800 dark:text-gray-400">Ready-to-use virtual labs to fit all your needs, be it for networking or ethical hacking simulations.</p>
 
               <div className="mt-7 grid gap-3 w-full sm:inline-flex">
-                <a className="inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800" href="#">
+                <Link className="inline-flex justify-center items-center gap-x-3 text-center bg-blue-600 hover:bg-blue-700 border border-transparent text-sm lg:text-base text-white font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:focus:ring-offset-gray-800" href="/signup">
                   Get started
                   <svg className="w-2.5 h-2.5" width="16" height="16" viewBox="0 0 16 16" fill="none">
                     <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
                   </svg>
-                </a>
+                </Link>
                 <a className="inline-flex justify-center items-center gap-x-3.5 text-sm lg:text-base text-center border hover:border-gray-300 shadow-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-white transition py-3 px-4 dark:border-gray-800 dark:hover:border-gray-600 dark:shadow-slate-700/[.7] dark:text-white dark:focus:ring-gray-700 dark:focus:ring-offset-gray-800" href="#">
                   Contact us
                 </a>
