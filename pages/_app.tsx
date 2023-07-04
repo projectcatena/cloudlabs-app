805e07542e34bee089da96e32805ee1607508901
loghogjog <s10223003@connect.np.edu.sg>
2023-07-04 23:50:48 +0800

    readded @/styles/css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,6 @@
-import { AppProps } from 'next/app';
+import '@/styles/globals.css'
+import type { AppProps } from 'next/app'
 
-function App({ Component, pageProps }: AppProps) {
-  
-  return (
-    <Component {...pageProps} />
-  );
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
 }
-
-export default App;
