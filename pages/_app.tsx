ce25a3deb9890e1b73a8a9fd9bb9aa096d610c53
loghogjog <s10223003@connect.np.edu.sg>
2023-07-04 23:32:41 +0800

    removed unused dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,9 @@
-import '@/styles/globals.css';
-import { createTheme } from "@mui/material";
 import { AppProps } from 'next/app';
 
-const theme = createTheme({});
-//{ Component, pageProps }: AppProps
 function App({ Component, pageProps }: AppProps) {
   
   return (
     <Component {...pageProps} />
-    
-  /*
-  <NoSSR>
-    <ThemeProvider theme={theme}>
-    <SnackbarProvider maxSnack={3}>
-        <Container maxWidth={false} disableGutters>
-          <BrowserRouter>
-            <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route path="/login" component={LoginPage} />
-              <Route path="/signup" component={SignUpPage} />
-              <ProtectedRoute path="/module" role={"USER"} component={ModulePage} />
-              <ProtectedRoute path="/users" role={"TUTOR"} component={UserPage} />
-              <ProtectedRoute path="/maindashboard" role={"USER"} component={MainDashboard} />
-              <ProtectedRoute path="/account_settings" role={"USER"} component={AccSettingsPage} />
-              <ProtectedRoute path="/console" role={"USER"} component={ConsolePage} />
-              <Redirect to={"/login"} />
-            </Switch>
-          </BrowserRouter>
-        </Container>
-    </SnackbarProvider>
-  </ThemeProvider>
-  </NoSSR>
-  */
   );
 }
 
