3ecbf4f0ca170b963f1d34aec59d5abb252a51f8
DancinParrot <kaizhetan@yahoo.com>
2023-07-27 11:21:51 +0800

    feat: use public image
    Added input fields for user to specify public image name and project

--- components/elements/Dropdown.tsx
+++ components/elements/Dropdown.tsx
@@ -2,10 +2,10 @@ import { useState } from "react";
 import StatusModal from "./VirtualMachineStatusModal";
 
 type DropdownProps = {
-        instanceName: string
+    instanceName: string
 }
 
-export default function Dropdown({ instanceName } : DropdownProps) {
+export default function Dropdown({ instanceName }: DropdownProps) {
     const [dropdown, setDropdown] = useState(false);
     const [isDelete, setIsDelete] = useState(false);
     const [StatusModalOpen, setStatusModalOpen] = useState(false);
@@ -20,8 +20,11 @@ export default function Dropdown({ instanceName } : DropdownProps) {
         try {
             const response = await fetch("http://localhost:8080/api/compute/delete", {
                 method: "POST",
+                credentials: "include",
                 headers: {
                     "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "*",
                 },
                 body: JSON.stringify(postData),
             });
@@ -44,7 +47,7 @@ export default function Dropdown({ instanceName } : DropdownProps) {
             instanceName,
         };
 
-        try{
+        try {
             const getStatusResponse = await fetch("http://localhost:8080/api/compute/status", {
                 method: "POST",
                 headers: {
@@ -65,20 +68,20 @@ export default function Dropdown({ instanceName } : DropdownProps) {
                 return;
             } else {
                 const response = await fetch("http://localhost:8080/api/compute/stop", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(postData),
-            });
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(postData),
+                });
 
-            if(!response.ok) {
-                throw new Error("Network response failed.");
-            }
+                if (!response.ok) {
+                    throw new Error("Network response failed.");
+                }
 
-            const result = await response.json();
+                const result = await response.json();
 
-            return result;
+                return result;
             }
         } catch (error) {
             console.log("Error:", error);
@@ -90,7 +93,7 @@ export default function Dropdown({ instanceName } : DropdownProps) {
             instanceName,
         };
 
-        try{
+        try {
             const getStatusResponse = await fetch("http://localhost:8080/api/compute/status", {
                 method: "POST",
                 headers: {
@@ -111,32 +114,32 @@ export default function Dropdown({ instanceName } : DropdownProps) {
                 return;
             } else {
                 const response = await fetch("http://localhost:8080/api/compute/start", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(postData),
-            });
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(postData),
+                });
 
-            if(!response.ok) {
-                throw new Error("Network response failed.");
-            }
+                if (!response.ok) {
+                    throw new Error("Network response failed.");
+                }
 
-            const result = await response.json();
+                const result = await response.json();
 
-            return result;
+                return result;
             }
         } catch (error) {
             console.log("Error:", error);
         }
     }
-        
+
     return (
         <div className="hs-dropdown relative inline-flex w-full">
             <button id="hs-dropdown-default" type="button" onClick={() => setDropdown((prev) => !prev)} className="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-br-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800">
                 Actions
                 <svg className="hs-dropdown-open:rotate-180 w-2.5 h-2.5 text-gray-600" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+                    <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
                 </svg>
             </button>
             {/* <button id="hs-dropdown-default" type="button" onClick={() => setDropdown((prev) => !prev)} className="hs-dropdown-toggle py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800">
@@ -145,7 +148,7 @@ export default function Dropdown({ instanceName } : DropdownProps) {
                 <path d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
                 </svg>
             </button> */}
-            <div className={`${dropdown? 'block absolute right-0 top-14': 'hidden'}`}>
+            <div className={`${dropdown ? 'block absolute right-0 top-14' : 'hidden'}`}>
                 <div className="transition-[opacity,margin] duration-[0.1ms] z-50 w-72 z-10 mt-2 min-w-[15rem] bg-white shadow-md rounded-lg p-2 dark:bg-gray-800 dark:border dark:border-gray-700 dark:divide-gray-700" aria-labelledby="hs-dropdown-default">
                     <button onClick={handleStart} className="w-full flex items-center gap-x-3.5 py-2 px-3 rounded-md text-sm text-gray-800 hover:bg-gray-100 focus:ring-2 focus:ring-blue-500 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-300">
                         Start
@@ -160,9 +163,9 @@ export default function Dropdown({ instanceName } : DropdownProps) {
                         Snapshots
                     </a>
                 </div>
- 
+
             </div>
             <StatusModal open={StatusModalOpen} onClose={() => setStatusModalOpen(false)} statusMessage={statusMessage} />
-       </div>
+        </div>
     )
 }
