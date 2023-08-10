8100932fb91597d08c787be7303a89fd9eedfc6e
Boon Chong Chuah <s10219129@connect.np.edu.sg>
2023-08-11 04:13:29 +0800

    fix: Dashboard should display the modules that are assigned to the user only

--- pages/modules/[slug].tsx
+++ pages/modules/[slug].tsx
@@ -142,7 +142,7 @@ export default function ModulePage({
                 :
 
                 <button type="button" className="py-2 px-3 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800">
-                  Contact your Teacher
+                  Contact your Lecturer
                 </button>
               )
             }
